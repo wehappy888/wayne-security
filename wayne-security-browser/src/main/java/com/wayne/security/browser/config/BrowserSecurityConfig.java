f58d7be8273f7467691e2644ca96d793cf8c9e1f
wayne <wehappy888@163.com>
2019-04-08 11:33:23 +0800

    20190408

---
+++ wayne-security-browser/src/main/java/com/wayne/security/browser/config/BrowserSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.wayne.security.browser.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wayne
+ * @date 2019/4/8
+ */
+@Component
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+}
---
+++ wayne-security-browser/src/main/java/com/wayne/security/browser/service/MyUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.wayne.security.browser.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author wayne
+ * @date 2019/4/8
+ */
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyUserDetailsService.class);
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.info("用户名：{}", username);
+        // 根据用户名查找用户信息
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("admin");
+        
+        String password = passwordEncoder.encode("123456");
+        logger.info("数据库密码:{}", password);
+
+        //User user = new User(username, password, authorities);
+
+        // 判断用户状态
+        User user = new User(username, password, true, true, true, true, authorities);
+        return user;
+    }
+}
